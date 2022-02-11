a2039e8a0644e189513eb10c3b405d61bf9c40b7
Alim Shogenov <shogenov@tutu.tech>
2022-02-11 23:04:16 +0300

    created start pod

---
+++ front/src/pages/Main/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styled';
+import { FC } from 'react';
+
+export const Main: FC = () => {
+    return (
+        <Container>
+            wevwv
+        </Container>
+    )
+}
---
+++ front/src/pages/Main/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #1e1e61;
+    width: 100%;
+    height: 100vh;
+`;
